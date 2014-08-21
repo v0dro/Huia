b6596604834140023e2a36c16ccfebfbcbcff4ef
James Harton <james@resistor.io>
2014-08-21 22:24:36 +1200

    No need for Huia#lex and Huia#parse any more.

--- lib/huia.rb
+++ lib/huia.rb
@@ -41,25 +41,4 @@ module Huia
     Huia::Script.new(cm, '(eval)')
   end
 
-  def lex string_or_io
-    lexer(string_or_io).tokens
-  end
-
-  def lexer string_or_io
-    str = if string_or_io.respond_to? :read
-            string_or_io.read
-          else
-            string_or_io
-          end
-    Huia::Lexer.new(str)
-  end
-
-  def parse string_or_io
-    parser(string_or_io).ast
-  end
-
-  def parser string_or_io
-    Huia::Parser.new lexer string_or_io
-  end
-
 end
