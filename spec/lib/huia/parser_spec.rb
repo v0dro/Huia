51f8bbe335a49764bde6ae268e1c85bafe8a6b25
James Harton <james@resistor.io>
2014-08-26 12:00:41 +1200

    Fix broken spec.

--- spec/lib/huia/parser_spec.rb
+++ spec/lib/huia/parser_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Huia::Parser do
   let(:source) { '' }
-  subject { Huia::Parser.new Huia::Lexer.new source }
+  subject { Huia::Parser.new Huia::Lexer.new source, '(eval)' }
 
   it { should be_a Racc::Parser }
 end
