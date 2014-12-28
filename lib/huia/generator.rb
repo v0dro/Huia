f0eba8c6b8a0efdc3d2c96a96d9873a7d33c8b5d
James Harton <james@resistor.io>
2014-12-29 09:54:54 +1300

    Add RDoc to `Huia::Generator`.

--- lib/huia/generator.rb
+++ lib/huia/generator.rb
@@ -1,6 +1,12 @@
 module Huia
+  # A subclass of ToolSet::Generator which adds a bunch of helpers
+  # for common Huia bytecode operations.
   class Generator < ToolSet::Generator
 
+    # Push a huia constant onto the stack.
+    # Huia constants are the core library
+    # and are located under Ruby's Huia::Core
+    # namespace.
     def push_huia_const const
       push_cpath_top
       find_const :Huia
@@ -8,6 +14,8 @@ module Huia
       find_const const.to_sym
     end
 
+    # Push a huia method call onto the stack via the Ruby
+    # Huia::Core::Object#__huia__send method.
     def huia_send method, arg_count=0
       push_literal method
       string_dup
@@ -15,6 +23,7 @@ module Huia
       send :__huia__send, arg_count + 1
     end
 
+    # Push a huia integer onto the stack.
     def push_huia_integer value
       push_huia_const :Integer
       huia_send 'createFromValue:', 1 do |g|
@@ -22,6 +31,7 @@ module Huia
       end
     end
 
+    # Push a huia float onto the stack.
     def push_huia_float value
       push_huia_const :Float
       huia_send 'createFromValue:', 1 do |g|
@@ -29,21 +39,27 @@ module Huia
       end
     end
 
+    # Push a huia false onto the stack.
     def push_huia_false
       push_huia_const :False
       huia_send 'create'
     end
 
+    # Push a huia true onto the stack.
     def push_huia_true
       push_huia_const :True
       huia_send 'create'
     end
 
+    # Push a huia nil onto the stack.
     def push_huia_nil
       push_huia_const :Nil
       huia_send 'create'
     end
 
+    # Push an empty huia array onto the stack
+    # and populate it with the contents of the block
+    # if asize is non-zero.
     def push_huia_array asize
       push_huia_const :Array
       huia_send 'createFromValue:', 1 do |g|
@@ -52,11 +68,13 @@ module Huia
       end
     end
 
+    # Push an empty Huia hash onto the stack.
     def push_huia_hash
       push_huia_const :Hash
       huia_send 'create'
     end
 
+    # Push a huia string onto the stack.
     def push_huia_string value
       push_huia_const :String
       huia_send 'createFromValue:', 1 do |g|
@@ -65,6 +83,8 @@ module Huia
       end
     end
 
+    # Push a huia closure onto the stack, passing
+    # in a block and possible argument names.
     def push_huia_closure block, argument_names=[]
       push_huia_const :Closure
       huia_send 'create:', 1 do |g|
